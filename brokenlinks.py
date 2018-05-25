f68d9d20e9fb37297328d2111accc4ffd98cea4c
Sarah Polzer <Sarah@Sarahs-MBP-2.fios-router.home>
2018-05-25 16:46:32 -0400

    broken links

--- brokenlinks.py
+++ brokenlinks.py
@@ -34,7 +34,7 @@ i = 0
    #requestlink=requests.get(hostname + "url")
   # i += 1
   # print("{}: {} - {}".format(i, requestlink.status_code, url[h_len:]))
-#all_links_list=[]
+all_links_list=[]
 r=0
 for url in urls:
     opens2 = requests.get(url)
@@ -43,16 +43,18 @@ for url in urls:
     #all_linkstwo=all_links.get('href')
     for link in all_links:
         # store links in variable
-        urltwo=link.get('href')
+          urltwo=link.get('href')
+          if urltwo not in all_links_list:
         # query each link for the status code
-        r+=1
-        try: 
-            request_links=requests.get(urltwo)
-            print("{}: {} - {}".format(r, request_links.status_code, urltwo[h_len:]))
-        except:
+            all_links_list.append(urltwo)
+for alllink in all_links_list:
+    try: 
+        request_links=requests.get(alllink)
+        print("{}: {} - {}".format(r, request_links.status_code, urltwo[h_len:]))
+    except:
         # requests.exceptions.RequestException(url2)
         # print('exception caught', url2 
-            pass
+        pass
     
             
      
